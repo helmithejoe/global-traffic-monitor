82cae395bf9d0200801655d6c5ae316acac9f1bf
Joe <jnes_0@yahoo.com>
2019-05-19 15:18:39 +0700

    add hour selection up to 7

--- app/views/traffic/display.php
+++ app/views/traffic/display.php
@@ -12,6 +12,10 @@
                 <option value="1">1</option>
                 <option value="2">2</option>
                 <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+                <option value="7">7</option>
             </select> hour(s).
         </form>
         <div id="chart_div" style="width: 900px; height: 500px;"></div>
